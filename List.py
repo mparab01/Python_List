446c6187ed0c3a349776123f38835581023cdbc9
mparab01 <infoeastindia1@gmail.com>
2016-09-30 22:02:35 -0500

    Added Python Files
    https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions

---
+++ List.py
@@ -0,0 +1,107 @@
+
+# coding: utf-8
+
+# In[1]:
+
+5/2
+
+
+# In[2]:
+
+from __future__ import division
+
+
+# In[3]:
+
+from __future__ import print_function
+
+
+# In[21]:
+
+list = range(10)
+
+
+# In[22]:
+
+list
+
+
+# In[23]:
+
+list
+
+
+# In[24]:
+
+list[2]
+
+
+# In[25]:
+
+list.reverse()
+
+
+# In[26]:
+
+list
+
+
+# In[27]:
+
+list.sort()
+
+
+# In[28]:
+
+list
+
+
+# In[29]:
+
+list.append(23)
+
+
+# In[30]:
+
+list
+
+
+# In[33]:
+
+list.index(23)
+
+
+# In[37]:
+
+list.insert(2, 5)
+
+
+# In[38]:
+
+list
+
+
+# In[39]:
+
+list.remove(23)
+
+
+# In[40]:
+
+list
+
+
+# In[41]:
+
+list.remove(5)
+
+
+# In[42]:
+
+list
+
+
+# In[ ]:
+
+
+
